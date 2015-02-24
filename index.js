d8d17a0bb587fc21b0a1fd42ad7f264d4fff58ff
Bryan Clark <clarkbw@gmail.com>
2015-02-23 21:30:52 -0500

    use the default property for the createAt and remove the instance function

--- index.js
+++ index.js
@@ -7,27 +7,21 @@ function timestamps(Model, options) {
   var createdAt = options.createdAt || 'createdAt';
   var updatedAt = options.updatedAt || 'updatedAt';
 
-  Model.defineProperty(createdAt, { type: Date, required : true });
+  Model.defineProperty(createdAt, { type: Date, required : true, default: new Date() });
   Model.defineProperty(updatedAt, { type: Date, required : true });
 
   Model.observe('before save', function event (ctx, next) {
     if (ctx.instance) {
       debug('%s.before save: %s', ctx.Model.modelName, ctx.instance.id);
-      Model.applyTimestamps(ctx.instance, ctx.instance.isNewRecord());
+      ctx.instance[updatedAt] = new Date();
       next();
     } else {
       debug('%s.before update matching %j', ctx.Model.pluralModelName, ctx.where);
-      Model.applyTimestamps(ctx.data, false);
+      ctx.data[updatedAt] = new Date();
       next();
     }
   });
 
-  Model.applyTimestamps = function(inst, creation) {
-    inst[updatedAt] = new Date();
-    if (creation) {
-      inst[createdAt] = inst[updatedAt];
-    }
-  };
 }
 
 module.exports = function mixin(app) {
