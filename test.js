0cb8fb9c49854325a425b89a51a19c75b981df3b
Bryan Clark <clarkbw@gmail.com>
2015-05-27 15:08:22 -0700

    trying out the tap testing system and adding coverage support

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
-/* jshint mocha: true */
-
-var assert = require('assert');
+var test = require('tap').test;
 
 var app = require('loopback');
 
@@ -15,68 +13,71 @@ var dataSource = app.createDataSource({
 // import our TimeStamp mixin
 require('./')(app);
 
-var Book;
-
-describe('loopback datasource timestamps', function() {
+test('loopback datasource timestamps', function(tap) {
   'use strict';
 
-  describe('createdAt', function() {
+  tap.test('createdAt', function(t) {
 
-    beforeEach(function(done) {
-      Book = dataSource.createModel('Book',
-        { name: String, type: String },
-        { mixins: {  TimeStamp: true } }
-        );
-      Book.destroyAll(done);
-    });
+    var Book = dataSource.createModel('Book',
+      { name: String, type: String },
+      { mixins: {  TimeStamp: true } }
+    );
 
-    it('should exist on create', function(done) {
-      Book.create({name: 'book 1', type: 'fiction'}, function(err, book) {
-        assert.ifError(err);
-        assert.ok(book.createdAt);
-        assert.equal(typeof book.createdAt, 'object');
-        assert.ok(book.createdAt instanceof Date);
-        done();
-      });
-    });
-
-    it('should not change on save', function(done) {
-      var createdAt;
-      Book.create({name:'book 1', type:'fiction'}, function(err, book) {
-        assert.ifError(err);
-        assert.ok(book.createdAt);
-        createdAt = book.createdAt;
-        book.name = 'book inf';
-        book.save(function(err, b) {
-          assert.equal(book.createdAt, b.createdAt);
-          done();
+    t.test('should exist on create', function(tt) {
+      Book.destroyAll(function() {
+        Book.create({name: 'book 1', type: 'fiction'}, function(err, book) {
+          tt.error(err);
+          tt.ok(book.createdAt);
+          tt.type(book.createdAt, Date);
+          tt.end();
         });
       });
     });
 
-    it('should not change on update', function(done) {
+    t.test('should not change on save', function(tt) {
       var createdAt;
-      Book.create({name:'book 1', type:'fiction'}, function(err, book) {
-        assert.ifError(err);
-        assert.ok(book.createdAt);
-        book.updateAttributes({ name:'book inf' }, function(err, b) {
-          assert.ifError(err);
-          assert.equal(book.createdAt, b.createdAt);
-          done();
+      Book.destroyAll(function() {
+        Book.create({name:'book 1', type:'fiction'}, function(err, book) {
+          tt.error(err);
+          tt.ok(book.createdAt);
+          createdAt = book.createdAt;
+          book.name = 'book inf';
+          book.save(function(err, b) {
+            tt.equal(book.createdAt, b.createdAt);
+            tt.end();
+          });
         });
       });
     });
 
-    it('should not change with bulk updates', function(done) {
+    t.test('should not change on update', function(tt) {
       var createdAt;
-      Book.create({name:'book 1', type:'fiction'}, function(err, book) {
-        assert.ifError(err);
-        assert.ok(book.createdAt);
-        Book.updateAll({ type:'fiction' }, { type:'non-fiction' }, function(err) {
-          assert.ifError(err);
-          Book.findById(book.id, function(err, b) {
-            assert.equal(book.createdAt.getTime(), b.createdAt.getTime());
-            done();
+      Book.destroyAll(function() {
+        Book.create({name:'book 1', type:'fiction'}, function(err, book) {
+          tt.error(err);
+          tt.ok(book.createdAt);
+          book.updateAttributes({ name:'book inf' }, function(err, b) {
+            tt.error(err);
+            tt.equal(book.createdAt, b.createdAt);
+            tt.end();
+          });
+        });
+      });
+    });
+
+    t.test('should not change with bulk updates', function(tt) {
+      var createdAt;
+      Book.destroyAll(function() {
+        Book.create({name:'book 1', type:'fiction'}, function(err, book) {
+          tt.error(err);
+          tt.ok(book.createdAt);
+          Book.updateAll({ type:'fiction' }, { type:'non-fiction' }, function(err) {
+            tt.error(err);
+            Book.findById(book.id, function(err, b) {
+              tt.error(err);
+              tt.equal(book.createdAt.getTime(), b.createdAt.getTime());
+              tt.end();
+            });
           });
         });
       });
@@ -84,165 +85,159 @@ describe('loopback datasource timestamps', function() {
 
   });
 
-  describe('updatedAt', function() {
+  tap.test('updatedAt', function(t) {
 
-    beforeEach(function(done) {
-      Book = dataSource.createModel('Book',
-        { name: String, type: String },
-        { mixins: {  TimeStamp: true } }
-        );
-      Book.destroyAll(done);
-    });
+    var Book = dataSource.createModel('Book',
+      { name: String, type: String },
+      { mixins: {  TimeStamp: true } }
+    );
 
-    it('should exist on create', function(done) {
-      Book.create({name:'book 1', type:'fiction'}, function(err, book) {
-        assert.ifError(err);
-        assert.ok(book.updatedAt);
-        assert.equal(typeof book.updatedAt, 'object');
-        assert.ok(book.updatedAt instanceof Date);
-        done();
+    t.test('should exist on create', function(tt) {
+      Book.destroyAll(function() {
+        Book.create({name:'book 1', type:'fiction'}, function(err, book) {
+          tt.error(err);
+          tt.ok(book.updatedAt);
+          tt.type(book.updatedAt, Date);
+          tt.end();
+        });
       });
     });
 
-    it('should be updated via updateAttributes', function(done) {
+    t.test('should be updated via updateAttributes', function(tt) {
       var updatedAt;
-      Book.create({name:'book 1', type:'fiction'}, function(err, book) {
-        assert.ifError(err);
-        assert.ok(book.updatedAt);
-        updatedAt = book.updatedAt;
+      Book.destroyAll(function() {
+        Book.create({name:'book 1', type:'fiction'}, function(err, book) {
+          tt.error(err);
+          tt.ok(book.updatedAt);
+          updatedAt = book.updatedAt;
 
-        // ensure we give enough time for the updatedAt value to be different
-        setTimeout(function pause() {
-          book.updateAttributes({ type:'historical-fiction' }, function(err, b) {
-            assert.ifError(err);
-            assert.ok(b.updatedAt);
-            assert.ok(b.updatedAt.getTime() > updatedAt.getTime());
-            done();
-          });
-        }, 1);
+          // ensure we give enough time for the updatedAt value to be different
+          setTimeout(function pause() {
+            book.updateAttributes({ type:'historical-fiction' }, function(err, b) {
+              tt.error(err);
+              tt.ok(b.updatedAt);
+              tt.ok(b.updatedAt.getTime() > updatedAt.getTime());
+              tt.end();
+            });
+          }, 1);
+        });
       });
     });
 
-    it('should update bulk model updates at once', function(done) {
+    t.test('should update bulk model updates at once', function(tt) {
       var createdAt1, createdAt2, updatedAt1, updatedAt2;
-      Book.create({name:'book 1', type:'fiction'}, function(err, book1) {
-        assert.ifError(err);
-        createdAt1 = book1.createdAt;
-        updatedAt1 = book1.updatedAt;
-        setTimeout(function pause1() {
-          Book.create({name:'book 2', type:'fiction'}, function(err, book2) {
-            assert.ifError(err);
-            createdAt2 = book2.createdAt;
-            updatedAt2 = book2.updatedAt;
-            assert.ok(updatedAt2.getTime() > updatedAt1.getTime());
-            setTimeout(function pause2() {
-              Book.updateAll({ type:'fiction' }, { type:'romance' }, function(err, count) {
-                assert.ifError(err);
-                assert.equal(createdAt1.getTime(), book1.createdAt.getTime());
-                assert.equal(createdAt2.getTime(), book2.createdAt.getTime());
-                Book.find({ type:'romance' }, function(err, books) {
-                  assert.ifError(err);
-                  assert.equal(books.length, 2);
-                  books.forEach(function(book) {
-                    // because both books were updated in the updateAll call
-                    // our updatedAt1 and updatedAt2 dates have to be less than the current
-                    assert.ok(updatedAt1.getTime() < book.updatedAt.getTime());
-                    assert.ok(updatedAt2.getTime() < book.updatedAt.getTime());
+      Book.destroyAll(function() {
+        Book.create({name:'book 1', type:'fiction'}, function(err, book1) {
+          tt.error(err);
+          createdAt1 = book1.createdAt;
+          updatedAt1 = book1.updatedAt;
+          setTimeout(function pause1() {
+            Book.create({name:'book 2', type:'fiction'}, function(err, book2) {
+              tt.error(err);
+              createdAt2 = book2.createdAt;
+              updatedAt2 = book2.updatedAt;
+              tt.ok(updatedAt2.getTime() > updatedAt1.getTime());
+              setTimeout(function pause2() {
+                Book.updateAll({ type:'fiction' }, { type:'romance' }, function(err, count) {
+                  tt.error(err);
+                  tt.equal(createdAt1.getTime(), book1.createdAt.getTime());
+                  tt.equal(createdAt2.getTime(), book2.createdAt.getTime());
+                  Book.find({ type:'romance' }, function(err, books) {
+                    tt.error(err);
+                    tt.equal(books.length, 2);
+                    books.forEach(function(book) {
+                      // because both books were updated in the updateAll call
+                      // our updatedAt1 and updatedAt2 dates have to be less than the current
+                      tt.ok(updatedAt1.getTime() < book.updatedAt.getTime());
+                      tt.ok(updatedAt2.getTime() < book.updatedAt.getTime());
+                    });
+                    tt.end();
                   });
-                  done();
                 });
-              });
-            }, 1);
-
-          });
-        }, 1);
+              }, 1);
+            });
+          }, 1);
+        });
       });
     });
 
   });
 
-  describe('boot options', function() {
+  tap.test('boot options', function(t) {
 
-    beforeEach(function(done) {
-      Book.destroyAll(done);
-    });
-
-    it('should use createdOn and updatedOn instead', function(done) {
-      Book = dataSource.createModel('Book',
+    t.test('should use createdOn and updatedOn instead', function(tt) {
+      var Book = dataSource.createModel('Book',
         { name: String, type: String },
         { mixins: {  TimeStamp: { createdAt:'createdOn', updatedAt:'updatedOn' } } }
-        );
-      Book.create({name:'book 1', type:'fiction'}, function(err, book) {
-        assert.ifError(err);
+      );
+      Book.destroyAll(function() {
+        Book.create({name:'book 1', type:'fiction'}, function(err, book) {
+          tt.error(err);
 
-        assert.ok(book.createdAt === undefined);
-        assert.ok(book.updatedAt === undefined);
+          tt.type(book.createdAt, 'undefined');
+          tt.type(book.updatedAt, 'undefined');
 
-        assert.ok(book.createdOn);
-        assert.equal(typeof book.createdOn, 'object');
-        assert.ok(book.createdOn instanceof Date);
+          tt.ok(book.createdOn);
+          tt.type(book.createdOn, Date);
 
-        assert.ok(book.updatedOn);
-        assert.equal(typeof book.updatedOn, 'object');
-        assert.ok(book.updatedOn instanceof Date);
-        done();
+          tt.ok(book.updatedOn);
+          tt.type(book.updatedOn, Date);
+
+          tt.end();
+        });
       });
     });
 
-    it('should default required on createdAt and updatedAt ', function(done) {
-      Book = dataSource.createModel('Book',
+    t.test('should default required on createdAt and updatedAt ', function(tt) {
+      var Book = dataSource.createModel('Book',
         { name: String, type: String },
         { mixins: {  TimeStamp: true } }
-        );
-      assert.equal(Book.definition.properties.createdAt.required, true);
-      assert.equal(Book.definition.properties.updatedAt.required, true);
-      done();
+      );
+      tt.equal(Book.definition.properties.createdAt.required, true);
+      tt.equal(Book.definition.properties.updatedAt.required, true);
+      tt.end();
     });
 
-    it('should have optional createdAt and updatedAt', function(done) {
-      Book = dataSource.createModel('Book',
+    t.test('should have optional createdAt and updatedAt', function(tt) {
+      var Book = dataSource.createModel('Book',
         { name: String, type: String },
         { mixins: {  TimeStamp: { required: false } } }
-        );
-      assert.equal(Book.definition.properties.createdAt.required, false);
-      assert.equal(Book.definition.properties.updatedAt.required, false);
-      done();
+      );
+      tt.equal(Book.definition.properties.createdAt.required, false);
+      tt.equal(Book.definition.properties.updatedAt.required, false);
+      tt.end();
     });
-    
+
   });
 
-  describe('operation hook options', function() {
+  tap.test('operation hook options', function(t) {
 
-    beforeEach(function() {
-      Book = dataSource.createModel('Book',
-        { name: String, type: String },
-        { mixins: {  TimeStamp: true } }
-        );
-    });
+    var Book = dataSource.createModel('Book',
+      { name: String, type: String },
+      { mixins: {  TimeStamp: true } }
+    );
 
-    afterEach(function(done) {
-      Book.destroyAll(done);
-    });
-
-    it('should skip changing updatedAt when option passed', function(done) {
+    t.test('should skip changing updatedAt when option passed', function(tt) {
       var updated, book;
-      Book.create({name:'book 1', type:'fiction'}, function(err, book1) {
-        assert.ifError(err);
+      Book.destroyAll(function() {
+        Book.create({name:'book 1', type:'fiction'}, function(err, book1) {
+          tt.error(err);
 
-        assert.ok(book1.updatedAt);
+          tt.ok(book1.updatedAt);
 
-        updated = book1.updatedAt;
-        book = book1.toObject();
-        book.name = 'book 2';
+          updated = book1.updatedAt;
+          book = book1.toObject();
+          book.name = 'book 2';
 
-        Book.updateOrCreate(book, {skipUpdatedAt: true}, function(err, book2) {
-          assert.ifError(err);
+          Book.updateOrCreate(book, {skipUpdatedAt: true}, function(err, book2) {
+            tt.error(err);
 
-          assert.ok(book2.updatedAt);
-          assert.equal(updated.getTime(), book2.updatedAt.getTime());
-          done();
+            tt.ok(book2.updatedAt);
+            tt.equal(updated.getTime(), book2.updatedAt.getTime());
+            tt.end();
+          });
+
         });
-
       });
     });
 
